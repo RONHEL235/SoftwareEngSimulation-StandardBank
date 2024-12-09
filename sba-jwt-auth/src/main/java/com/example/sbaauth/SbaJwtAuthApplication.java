6faeadb7b4e022fb70c055702dad557385917d68
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-12-09 14:47:27 +0200

    Imported the java spring boot project

---
+++ sba-jwt-auth/src/main/java/com/example/sbaauth/SbaJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbaauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbaJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbaJwtAuthApplication.class, args);
+	}
+
+}
---
+++ sba-jwt-auth/src/test/java/com/example/sbaauth/SbaJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbaauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbaJwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

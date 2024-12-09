2d66a17a415efaa815f79d47aea12cd18f733ee2
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-12-09 20:03:07 +0200

    The main method serves as the application's entry point

--- sba-jwt-auth/src/main/java/com/example/sbaauth/SbaJwtAuthApplication.java
+++ sba-jwt-auth/src/main/java/com/example/sbaauth/SbaJwtAuthApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbaJwtAuthApplication {
-
+	
+// The main method serves as the application's entry point
 	public static void main(String[] args) {
 		SpringApplication.run(SbaJwtAuthApplication.class, args);
 	}

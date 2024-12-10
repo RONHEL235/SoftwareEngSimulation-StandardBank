fd86d32d4e979714af8265b949d109945b0d5459
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-12-10 21:23:27 +0200

    A simple test to verify that the Spring application context loads successfully

--- sba-jwt-auth/src/test/java/com/example/sbaauth/SbaJwtAuthApplicationTests.java
+++ sba-jwt-auth/src/test/java/com/example/sbaauth/SbaJwtAuthApplicationTests.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SbaJwtAuthApplicationTests {
 
+	// A simple test to verify that the Spring application context loads successfully
 	@Test
 	void contextLoads() {
 	}

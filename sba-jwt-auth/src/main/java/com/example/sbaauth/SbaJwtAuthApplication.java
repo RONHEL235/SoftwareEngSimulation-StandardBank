b593605ef9872212fecf31147dc596b956fbcc98
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-12-10 21:25:30 +0200

    Marking this class as the main entry point for the Spring Boot application

--- sba-jwt-auth/src/main/java/com/example/sbaauth/SbaJwtAuthApplication.java
+++ sba-jwt-auth/src/main/java/com/example/sbaauth/SbaJwtAuthApplication.java
@@ -5,6 +5,7 @@ package com.example.sbaauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Marks this class as the main entry point for the Spring Boot application
 @SpringBootApplication
 public class SbaJwtAuthApplication {
 

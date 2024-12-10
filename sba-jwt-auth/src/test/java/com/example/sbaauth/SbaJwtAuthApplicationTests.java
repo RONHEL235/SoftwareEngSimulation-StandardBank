ed9929f473e722d3a5d65e934fee48e2c22ae5c8
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-12-10 21:21:01 +0200

    Importing necessary JUnit and Spring Boot test classes

--- sba-jwt-auth/src/test/java/com/example/sbaauth/SbaJwtAuthApplicationTests.java
+++ sba-jwt-auth/src/test/java/com/example/sbaauth/SbaJwtAuthApplicationTests.java
@@ -1,6 +1,7 @@
 // Specifies the package for organizing the test class
 package com.example.sbaauth;
 
+// Importing necessary JUnit and Spring Boot test classes
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
